b5fb5d19a2e61330c65cb3367883333c2ee177e8
Malith <malithwwa@gmail.com>
2026-07-22 19:14:03 +0530

    test husky with npx in pre-commit file

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
    return (
 <div className="p-4">
          <p className="font-bold p-4 text-3xl">{message}</p>
-         <Button>Button</Button>
+                     <Button>Button</Button>
 </div>
                     )
 }
