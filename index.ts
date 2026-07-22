9075f08cbc2fec38a8d4a2f55ea9d94a19f0d51b
Malith <malithwwa@gmail.com>
2026-07-22 18:26:59 +0530

    run both apps together

---
+++ index.ts
@@ -0,0 +1,6 @@
+import concurrently from "concurrently";
+
+concurrently([
+  { command: "bun run dev", name: "server", cwd: "packages/server", prefixColor: "blue" },
+  { command: "bun run dev", name: "client", cwd: "packages/client", prefixColor: "green" }
+]);
