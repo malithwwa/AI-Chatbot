4713343dd80cd7c867c57834707ea9937509889a
Malith <malithwwa@gmail.com>
2026-07-23 11:47:22 +0530

    extract to MVC

---
+++ packages/server/controllers/chat.controller.ts
@@ -0,0 +1,34 @@
+import z from 'zod';
+import { chatService } from '../services/chat.service';
+import type { Request, Response } from 'express';
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, { message: 'Prompt cannot be empty' })
+      .max(1000, { message: 'Prompt cannot exceed 1000 characters' }),
+   conversationId: z.string(),
+});
+
+export const chatController = {
+   sendMessage: async (req: Request, res: Response) => {
+      const parsedResult = chatSchema.safeParse(req.body);
+      if (!parsedResult.success) {
+         return res.status(400).json(parsedResult.error.format());
+      }
+      try {
+         const { prompt, conversationId } = req.body;
+         const response = await chatService.sendMessage(prompt, conversationId);
+
+         res.json({
+            message: response.message,
+         });
+      } catch (error) {
+         res.status(500).json({
+            error: 'Error fetching chat completion:',
+            details: error,
+         });
+      }
+   },
+};
---
+++ packages/server/services/chat.service.ts
@@ -0,0 +1,29 @@
+import OpenAI from 'openai';
+import { conversationRepository } from '../repository/conversation.repository';
+
+type ChatService = {
+   id: string;
+   message: string;
+};
+
+const client = new OpenAI({
+   baseURL: 'https://openrouter.ai/api/v1',
+   apiKey: process.env.OPENROUTER_API_KEY,
+});
+
+export const chatService = {
+   sendMessage: async (prompt: string, conversationId: string):Promise<ChatService> => {
+      const response = await client.responses.create({
+         model: 'openai/gpt-oss-20b:free',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 100,
+         // previous_response_id: conversationRepository.getLastResponseId(conversationId),
+      });
+      conversationRepository.setLastResponseId(conversationId, response.id);
+      return {
+            id: response.id,
+            message: response.output_text,
+      };
+   },
+};
