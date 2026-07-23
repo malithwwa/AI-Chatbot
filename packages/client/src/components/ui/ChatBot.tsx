863af02d42944746ee473e5ef54d9f7729b2e3c7
Malith <malithwwa@gmail.com>
2026-07-23 14:02:02 +0530

    validate & submit form

--- packages/client/src/components/ui/ChatBot.tsx
+++ packages/client/src/components/ui/ChatBot.tsx
@@ -1,17 +1,42 @@
 import { Button } from './button';
 import { FaArrowUp } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+   prompt: string;
+};
 const ChatBot = () => {
+   const { register, handleSubmit, reset, formState } = useForm<FormData>({});
+   
+   const onSubmit = (data: FormData) => {
+      console.log(data);
+      reset();
+   };
+
+   const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+  
+            if (e.key === 'Enter' && !e.shiftKey) {
+               e.preventDefault();
+               handleSubmit(onSubmit)();
+            }
+         } 
+  
    return (
-      <div className="flex flex-col gap-2 items-end border-2 border-gray-300 p-4 rounded-3xl">
+      <form
+         onSubmit={handleSubmit(onSubmit)}
+         onKeyDown={onKeyDown}
+         className="flex flex-col gap-2 items-end border-2 border-gray-300 p-4 rounded-3xl"
+      >
          <textarea
+            {...register('prompt', { required: true, validate: (value) => value.trim().length > 0 })}
             placeholder="Ask anything"
             maxLength={1000}
             className="w-full border-0 focus:outline-0 resize-none"
          />
-         <Button className="rounded-full w-9 h-9">
+         <Button disabled={!formState.isValid} type="submit" className="rounded-full w-9 h-9">
             <FaArrowUp />
          </Button>
-      </div>
+      </form>
    );
 };
 
