1d9a47af3c466dfc10a488fe45d88c0803d41182
Malith <malithwwa@gmail.com>
2026-07-28 19:56:24 +0530

    fix issues

--- packages/server/services/chat.service.ts
+++ packages/server/services/chat.service.ts
@@ -2,23 +2,33 @@ import fs from 'node:fs';
 import path from 'node:path';
 import OpenAI from 'openai';
 import { conversationRepository } from '../repositories/conversation.repository';
-import template from '../prompts/chatbot.txt'
+// import template from '../prompts/chatbot.txt'
 
 type ChatResponse = {
    id: string;
    message: string;
 };
 
+const promptPath = path.join(__dirname, '../prompts/chatbot.txt');
+
+const chatbotPrompt = fs.readFileSync(promptPath, 'utf-8');
+
 const client = new OpenAI({
    baseURL: 'https://openrouter.ai/api/v1',
    apiKey: process.env.OPENROUTER_API_KEY,
 });
 
-const parkInfo = fs.readFileSync(path.join(__dirname, '..', 'prompts', 'WonderWorld.md'), 'utf-8')
-const instructions =template.replace('{{parkInfo}}', parkInfo)
+const parkInfo = fs.readFileSync(
+   path.join(__dirname, '..', 'prompts', 'WonderWorld.md'),
+   'utf-8'
+);
+const instructions = chatbotPrompt.replace('{{parkInfo}}', parkInfo);
 
 export const chatService = {
-   sendMessage: async (prompt: string, conversationId: string):Promise<ChatResponse> => {
+   sendMessage: async (
+      prompt: string,
+      conversationId: string
+   ): Promise<ChatResponse> => {
       const response = await client.responses.create({
          model: 'openai/gpt-oss-20b:free',
          instructions,
@@ -29,8 +39,8 @@ export const chatService = {
       });
       conversationRepository.setLastResponseId(conversationId, response.id);
       return {
-            id: response.id,
-            message: response.output_text,
+         id: response.id,
+         message: response.output_text,
       };
    },
 };
