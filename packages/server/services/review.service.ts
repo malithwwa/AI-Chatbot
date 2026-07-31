78668e4fee727e984133543ebae11f2969ac3415
Malith <malithwwa@gmail.com>
2026-07-31 19:00:19 +0530

    extract the prompt

--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,8 +1,22 @@
+import  fs  from 'node:fs';
 import OpenAI from 'openai';
 import type { Review } from '../generated/prisma/client';
 import { prisma } from '../index';
 import { reviewRepository } from '../repositories/review.repository';
 import { llmClient } from '../llm/client';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const promptPath = path.join(__dirname, '../prompts/summarize-review.txt');
+const reviewPrompt = fs.readFileSync(promptPath, 'utf-8');
+
+// const parkInfo = fs.readFileSync(
+//    path.join(__dirname, '..', 'prompts', 'WonderWorld.md'),
+//    'utf-8'
+// );
+
 
 export const reviewService = {
    getReviews: async (productId: number): Promise<Review[]> => {
@@ -11,11 +25,7 @@ export const reviewService = {
    summarizeReviews: async (productId: number): Promise<string> => {
       const reviews = reviewRepository.getReviews(productId, 10);
       const joinedReviews = (await reviews).map((r) => r.content).join('\n\n');
-      const prompt = `
-      Summarize  the follwing customer reviews into a short paragraph highlighting key themes, both positive and negative:
-
-      ${joinedReviews}
-      `;
+      const prompt = reviewPrompt.replace('{{reviews}}', joinedReviews);
 
       const response = await llmClient.generateText({prompt, maxTokens:500, temperature:0.2})
       return response.text;
