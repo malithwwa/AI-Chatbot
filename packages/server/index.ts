696dbbbac64e6b42931dc0bfb5f5d057400a99d1
Malith <malithwwa@gmail.com>
2026-07-31 13:32:16 +0530

    create fetching reviews api

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes/routes.js';
-import cors from 'cors';   
+import cors from 'cors';
+import { PrismaClient } from './generated/prisma/client.js';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+
 dotenv.config();
 
 const app = express();
 // Allow requests from React/Vite
 app.use(
    cors({
-      origin:  process.env.CLIENT_URL || 'http://localhost:5173',
+      origin: process.env.CLIENT_URL || 'http://localhost:5173',
    })
 );
 app.use(express.json());
@@ -16,6 +19,31 @@ app.use('/api', router);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-   console.log(`Server is running on http://localhost:${port}`);
+const adapter = new PrismaMariaDb({
+   host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+
+  connectionLimit: 5,
 });
+export const prisma = new PrismaClient({ adapter });
+
+async function startServer() {
+   try {
+      // Test the database connection
+      await prisma.$connect();
+      console.log('✅ Database connected successfully.');
+
+      app.listen(port, () => {
+         console.log(`Server is running on http://localhost:${port}`);
+      });
+   } catch (error) {
+      console.error('❌ Failed to connect to the database.');
+      console.error(error);
+      process.exit(1); // Stop the server if DB connection fails
+   }
+}
+
+startServer();
--- packages/server/routes/routes.ts
+++ packages/server/routes/routes.ts
@@ -1,5 +1,7 @@
 import express, { type Request, type Response } from 'express';
 import { chatController } from '../controllers/chat.controller.js';
+import { prisma } from '../index.js';
+
 
 const router = express.Router();
 router.get('/', (req: Request, res: Response) => {
@@ -11,5 +13,23 @@ router.get('/hello', (req: Request, res: Response) => {
 });
 
 router.post('/chat', chatController.sendMessage);
+
+router.get('/products/:id/reviews', async(req: Request, res: Response) => {
+
+   const productId = Number(req.params.id);
+
+   if (isNaN(productId)){
+      res.status(400).json({error:'Invalid Product Id'})
+      return;
+   }
+
+   // dont need to write raw sql, instead object of our application
+   const reviews = await prisma.review.findMany({
+      where: { productId },
+      orderBy: { createdAt: 'desc'}
+   });
+
+   res.send({ data: reviews });
+})
 
 export default router;
